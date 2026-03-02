819a7a6321be8220adf879a1f1cebfd4fd0ecadb
Ander699 <jhonrue28@gmail.com>
2026-03-02 12:43:51 -0500

    feat: :sparkles: añadí la funcionalidad al archivo .py

---
+++ app.py
@@ -0,0 +1,4 @@
+print ("Hola bievenido a la calculadora básica de sumas")
+num1=int (input ("Ingrese el primer número: "))
+num2=int (input ("Ingrese el segundo número: "))
+print ("El resultado de la suma es: ",num1+num2)
