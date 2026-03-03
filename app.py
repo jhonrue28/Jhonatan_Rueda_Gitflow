03bae678ad957cbdbe90f65adaa6d4dced004d7a
Ander699 <jhonrue28@gmail.com>
2026-03-02 22:00:26 -0500

    feat: :sparkles: Añadí menú confirmación

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ num1=int (input ("Ingrese el primer número: "))
 num2=int (input ("Ingrese el segundo número: "))
 print ("El resultado de la suma es: ",num1+num2)
 print ("pq usas un programa para hacer una mukltiplicación?🤡")
+print ("¿En serio?¿No sabes multiplicar?🤡")
+decision=int(input("1.Si, 2.No"))
+if decision==1:
+    print ("hola")
+elif decision==2:
+    print ("Adiós")
+else:
+    print ("Opción no válida")
